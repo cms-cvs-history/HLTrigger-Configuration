40bddbcea06eed5ff55396cc1f0cc08551e68f46
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2007-08-03 02:13:51 +0000

    Let the script state what it is doing.

--- test/runMe.sh
+++ test/runMe.sh
@@ -2,5 +2,12 @@
 
 eval `scramv1 runtime -csh`
 rehash
-cmsRun ConvertDigiToRaw.cfg >& ConvertDigiToRaw.log
-cmsRun HLTtable.cfg         >& HLTtable.log
+
+echo "rm -i myDigiToRaw.root ConvertDigiToRaw.log HLTtable.log"
+      rm -i myDigiToRaw.root ConvertDigiToRaw.log HLTtable.log
+
+echo "cmsRun ConvertDigiToRaw.cfg >& ConvertDigiToRaw.log"
+      cmsRun ConvertDigiToRaw.cfg >& ConvertDigiToRaw.log
+
+echo "cmsRun HLTtable.cfg         >& HLTtable.log"
+      cmsRun HLTtable.cfg         >& HLTtable.log
