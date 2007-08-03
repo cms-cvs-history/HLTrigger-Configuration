3a4111484d69627074c99192f4186c282f73ee9b
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2007-08-03 21:10:35 +0000

    *** empty log message ***

--- test/runMe.sh
+++ test/runMe.sh
@@ -3,14 +3,21 @@
 eval `scramv1 runtime -csh`
 rehash
 
+echo " "
 echo "rm -i my*To*.root Convert*To*.log HLTtable.log"
       rm -i my*To*.root Convert*To*.log HLTtable.log
 
+echo " "
 echo "cmsRun --strict ConvertDigiToRaw.cfg >& ConvertDigiToRaw.log"
       cmsRun --strict ConvertDigiToRaw.cfg >& ConvertDigiToRaw.log
 
+echo " "
 echo "cmsRun --strict ConvertRawToDigi.cfg >& ConvertRawToDigi.log"
       cmsRun --strict ConvertRawToDigi.cfg >& ConvertRawToDigi.log
 
+echo " "
 echo "cmsRun --strict HLTtable.cfg         >& HLTtable.log"
       cmsRun --strict HLTtable.cfg         >& HLTtable.log
+
+echo " "
+echo "Finished!"
