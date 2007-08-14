1ff0d9fa0848db0e73c375c2da4df0046c47dc2f
cmsbuild <>
2007-08-14 23:41:29 +0000

    This commit was manufactured by cvs2git to create tag 'HLTrigger- Configuration-V00-02-09'.
    Sprout from master 2007-07-06 11:31:36 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> 'Config file and module name rationalisation.'
    Cherrypick from master 2007-08-14 23:41:28 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> '*** empty log message ***':
        data/common/Digi.cff
        data/common/HLTPrescaleReset.cff
        data/common/HLTSetupCase1.cff
        data/common/HLTSetupCase2.cff
        data/common/HLTSetupCase3.cff
        data/common/HLTSetupCase4.cff
        data/common/HLTSetupCommon.cff
        data/main/Famos.cff
        data/main/OfflineReco.cff
        data/rawToDigi/DTDBCabling.cff
        data/rawToDigi/EcalRecord.cff
        data/rawToDigi/GctDigiToRaw.cff
        data/rawToDigi/GctRawToDigi.cff
        data/rawToDigi/GtDigiToRaw.cff
        data/rawToDigi/GtRawToDigi.cff
        data/rawToDigi/RPCDBCabling.cff
        data/rawToDigi/SiPixelDBCabling.cff
        test/ConvertDigiToRaw.cfg
        test/ConvertRawToDigi.cfg
        test/RelVal_Digi_Digi2Raw.cfg
        test/RelVal_HLTFromRaw.cfg
        test/RelVal_Reco.cfg
        test/runMe.sh
        test/testcfg

---
+++ test/runMe.sh
@@ -0,0 +1,51 @@
+#!/bin/tcsh
+
+eval `scramv1 runtime -csh`
+rehash
+
+echo " "
+echo " Quick tests (three jobs: digi2raw, raw2digi, HLT from raw):"
+
+echo " "
+echo "/bin/rm myDigiToRaw.root                ConvertDigiToRaw.log"
+      /bin/rm myDigiToRaw.root                ConvertDigiToRaw.log
+echo "cmsRun --strict ConvertDigiToRaw.cfg >& ConvertDigiToRaw.log"
+      cmsRun --strict ConvertDigiToRaw.cfg >& ConvertDigiToRaw.log
+
+echo " "
+echo "/bin/rm myRawToDigi.root                ConvertRawToDigi.log"
+      /bin/rm myRawToDigi.root                ConvertRawToDigi.log
+echo "cmsRun --strict ConvertRawToDigi.cfg >& ConvertRawToDigi.log"
+      cmsRun --strict ConvertRawToDigi.cfg >& ConvertRawToDigi.log
+
+echo " "
+echo "/bin/rm HLTPoolOutput.root              HLTtable.log"
+      /bin/rm HLTPoolOutput.root              HLTtable.log
+echo "cmsRun --strict HLTtable.cfg         >& HLTtable.log"
+      cmsRun --strict HLTtable.cfg         >& HLTtable.log
+
+
+echo " "
+echo " Production chain (three jobs: digi+digi2raw, HLT, reco):"
+
+echo " "
+echo "/bin/rm myDigiToRaw.root                RelVal_Digi_Digi2Raw.log"
+      /bin/rm myDigiToRaw.root                RelVal_Digi_Digi2Raw.log
+echo "./testcfg RelVal_Digi_Digi2Raw.cfg   >& RelVal_Digi_Digi2Raw.log"
+      ./testcfg RelVal_Digi_Digi2Raw.cfg   >& RelVal_Digi_Digi2Raw.log
+
+echo " "
+echo "/bin/rm HLTPoolOutput.root              RelVal_HLTFromRaw.log"
+      /bin/rm HLTPoolOutput.root              RelVal_HLTFromRaw.log
+echo "./testcfg RelVal_HLTFromRaw.cfg      >& RelVal_HLTFromRaw.log"
+      ./testcfg RelVal_HLTFromRaw.cfg      >& RelVal_HLTFromRaw.log
+
+echo " "
+echo "/bin/rm reco.root                       RelVal_Reco.log"
+      /bin/rm reco.root                       RelVal_Reco.log
+echo "./testcfg RelVal_Reco.cfg            >& RelVal_Reco.log"
+      ./testcfg RelVal_Reco.cfg            >& RelVal_Reco.log
+
+
+echo " "
+echo "Finished!"
