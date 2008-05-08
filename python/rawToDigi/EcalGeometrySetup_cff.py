2741f85e1ee24e4660ec432c8bad776ee28103cf
cmsbuild <>
2008-05-08 15:27:08 +0000

    This commit was manufactured by cvs2git to create tag 'HLTrigger- Configuration-V00-07-37'.
    Sprout from master 2008-05-08 15:27:07 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> 'More fixes but not quite there yet!'
    Delete:
        python/common/CaloTowers_cff.py
        python/common/EcalRegionalRecoFromRaw_cff.py
        python/common/HLTEndpath_cff.py
        python/common/HLTFullReco_cff.py
        python/common/HLTSetupCase2_cff.py
        python/common/HLTSetupCommon_cff.py
        python/common/HLTSetupFromRaw_cff.py
        python/common/HLTSetup_cff.py
        python/common/RecoJetMETCor1_cff.py
        python/common/RecoJetMET_cff.py
        python/common/TrackerTracks_cff.py
        python/common/Vertexing_cff.py
        python/main/BTau_cff.py
        python/main/Egamma_cff.py
        python/main/HLTpaths_cff.py
        python/main/HLTtable_cff.py
        python/main/JetMET_cff.py
        python/main/Muon_cff.py
        python/main/Special_cff.py
        python/main/Xchannel_cff.py
        python/rawToDigi/CSCRawToDigi_cff.py
        python/rawToDigi/DTRawToDigi_cff.py
        python/rawToDigi/EcalESRawToDigi_cff.py
        python/rawToDigi/EcalGeometrySetup_cff.py
        python/rawToDigi/EcalRawToDigi_cff.py
        python/rawToDigi/EcalRecord_cff.py
        python/rawToDigi/FakeConditions_cff.py
        python/rawToDigi/FrontierConditions_DevDB_cff.py
        python/rawToDigi/FrontierConditions_IntDB_cff.py
        python/rawToDigi/FrontierConditions_cff.py
        python/rawToDigi/HcalRawToDigi_cff.py
        python/rawToDigi/RPCRawToDigi_cff.py
        python/rawToDigi/RawToDigi_cff.py
        python/rawToDigi/SetupRawToDigi_cff.py
        python/rawToDigi/SiPixelRawToDigi_cff.py
        python/rawToDigi/SiStripRawToClusters_cff.py
        python/rawToDigi/frontierCablingSetup_cff.py
        python/rawToDigi/sqliteCablingSetup_cff.py

--- python/rawToDigi/EcalGeometrySetup_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from Geometry.EcalMapping.EcalMapping_cfi import *
-from MagneticField.Engine.volumeBasedMagneticField_cfi import *
-from Geometry.CaloEventSetup.CaloGeometry_cfi import *
-from Geometry.CaloEventSetup.EcalTrigTowerConstituents_cfi import *
-from Geometry.CMSCommonData.cmsIdealGeometryXML_cfi import *
-
