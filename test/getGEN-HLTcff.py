959713ca248ef3ef1be07df4c38122d05c4eda9b
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-05-16 07:54:04 +0000

    Fixes to make HLT work in pre4, and removing AlCaPiZero path which crashes.

--- test/getGEN-HLTcff.py
+++ test/getGEN-HLTcff.py
@@ -132,7 +132,7 @@ else:
     esmodules += "-HcalTopologyIdealEP"
     
     modules = "--modules -hltTriggerSummaryAOD"
-    paths   = "--paths -CandHLT1MuonTrackerNonIso"
+    paths   = "--paths -CandHLT1MuonTrackerNonIso,-AlCaEcalPi0"
 
     services = "--services "
     services += "-PrescaleService,"
