0361c7dd2ffb05cdb664288efb53e4c7ad6102c3
cmsbuild <>
2008-05-23 09:08:55 +0000

    This commit was manufactured by cvs2git to create tag 'HLTrigger- Configuration-V00-07-26-17'.
    Sprout from master 2008-05-02 07:16:41 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> 'Update event content and global tag (V2).'
    Cherrypick from master 2008-04-17 10:55:19 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> 'Chaning global tag to STARTUP_V1 (as used in CSA08).':
        data/rawToDigi/RawToDigi.cff
        python/HLTrigger_EventContent_cff.py
        python/HLTrigger_PrimaryDatasets_cff.py
        python/__init__.py
        test/RelVal_GT_DigiToRawFrom17XRaw.cfg
    Cherrypick from master 2008-05-22 17:50:34 UTC Bryan Dahmes <sha1-ad386f655f6fb04a44f83e832ee96bb49b44ff9d@cern.ch> 'Modifying ConfDB access for 20X':
        data/HLT_2E30.cff
        test/TTbarGenHLT.cfg
        test/TTbarGenSim.cfg
    Cherrypick from master 2008-05-23 09:08:49 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> '*** empty log message ***':
        test/HLT_2E30.cff
        test/OnLine_HLTFromRaw_2E30.cfg
    Cherrypick from master 2008-05-22 20:10:08 UTC Bryan Dahmes <sha1-ad386f655f6fb04a44f83e832ee96bb49b44ff9d@cern.ch> 'Make getHLTcff.py executable':
        test/getHLTcff.py
    Delete:
        python/HLT_1E32_cff.py
        python/HLT_2E30_cff.py
        python/common/CaloTowers_cff.py
        python/common/EcalRegionalRecoFromRaw_cff.py
        python/common/HLTEndpath_cff.py
        python/common/HLTFullReco_cff.py
        python/common/HLTSetupCase2_cff.py
        python/common/HLTSetupCommon_cff.py
        python/common/HLTSetupFromRaw_cff.py
        python/common/HLTSetup_cff.py
        python/common/RecoJetMETCor1_cff.py
        python/common/RecoJetMET_cff.py
        python/common/TrackerTracks_cff.py
        python/common/Vertexing_cff.py
        python/main/BTau_cff.py
        python/main/Egamma_cff.py
        python/main/HLTpaths_cff.py
        python/main/HLTtable_cff.py
        python/main/JetMET_cff.py
        python/main/Muon_cff.py
        python/main/Special_cff.py
        python/main/Xchannel_cff.py
        python/rawToDigi/CSCRawToDigi_cff.py
        python/rawToDigi/DTRawToDigi_cff.py
        python/rawToDigi/EcalESRawToDigi_cff.py
        python/rawToDigi/EcalGeometrySetup_cff.py
        python/rawToDigi/EcalRawToDigi_cff.py
        python/rawToDigi/EcalRecord_cff.py
        python/rawToDigi/FakeConditions_cff.py
        python/rawToDigi/FrontierConditions_DevDB_cff.py
        python/rawToDigi/FrontierConditions_IntDB_cff.py
        python/rawToDigi/FrontierConditions_cff.py
        python/rawToDigi/HcalRawToDigi_cff.py
        python/rawToDigi/RPCRawToDigi_cff.py
        python/rawToDigi/RawToDigi_cff.py
        python/rawToDigi/SetupRawToDigi_cff.py
        python/rawToDigi/SiPixelRawToDigi_cff.py
        python/rawToDigi/SiStripRawToClusters_cff.py
        python/rawToDigi/frontierCablingSetup_cff.py
        python/rawToDigi/sqliteCablingSetup_cff.py
        test/getGEN-HLTcff.py

---
+++ test/getHLTcff.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+
+# Usage: ./getHLTcff.py <Version from ConfDB> <Name of cff file> <process Name>
+
+import sys
+import os
+import commands
+import getopt
+import fileinput
+
+def usage():
+    print "Usage: ./getHLTcff.py <Version from ConfDB> <Name of cff> <cff use case>"
+    print "If \"GEN-HLT\" is specified for cff use case, a stripped HLT.cff is generated for the GEN-HLT workflow"
+    print "The default is to create an HLT.cff from the ConfDB with minimal modifications for validation"
+    sys.exit(1)
+
+argc = len(sys.argv)
+
+useCase = "ONLINE"
+if argc == 3:
+    dbName = sys.argv[1]
+    cffName = sys.argv[2]
+elif argc == 4:
+    dbName = sys.argv[1]
+    cffName = sys.argv[2]
+    useCase = sys.argv[3]
+else:
+    usage()
+
+if os.path.exists(cffName):
+    print cffName, "already exists"
+else:
+    # Initialize everything
+    essources = "  " 
+    esmodules = "  " 
+    
+    if useCase == "GEN-HLT":
+        essources = "--essources "
+        essources += "-GlobalTag,"
+        essources += "-HepPDTESSource,"
+        essources += "-L1GtBoardMapsRcdSource,"
+        essources += "-L1GtParametersRcdSource,"
+        essources += "-L1GtPrescaleFactorsAlgoTrigRcdSource,"
+        essources += "-L1GtPrescaleFactorsTechTrigRcdSource,"
+        essources += "-L1GtStableParametersRcdSource,"
+        essources += "-L1GtTriggerMaskAlgoTrigRcdSource,"
+        essources += "-L1GtTriggerMaskTechTrigRcdSource,"
+        essources += "-L1GtTriggerMaskVetoAlgoTrigRcdSource,"
+        essources += "-L1GtTriggerMaskVetoTechTrigRcdSource,"
+        essources += "-L1MuTriggerScalesRcdSource,"
+        essources += "-SiStripQualityFakeESSource,"
+        essources += "-XMLIdealGeometryESSource,"
+        essources += "-eegeom,"
+        essources += "-emrcdsrc,"
+        essources += "-es_hardcode,"
+        essources += "-jetrcdsrc,"
+        essources += "-l1CaloGeomRecordSource,"
+        essources += "-magfield"
+
+        esmodules = "--esmodules "
+        esmodules += "-CSCGeometryESModule,"
+        esmodules += "-CaloGeometryBuilder,"
+        esmodules += "-CaloTowerHardcodeGeometryEP,"
+        esmodules += "-DTGeometryESModule,"
+        esmodules += "-EcalBarrelGeometryEP,"
+        esmodules += "-EcalElectronicsMappingBuilder,"
+        esmodules += "-EcalEndcapGeometryEP,"
+        esmodules += "-EcalLaserCorrectionService,"    
+        esmodules += "-EcalPreshowerGeometryEP,"
+        esmodules += "-HcalHardcodeGeometryEP,"
+        esmodules += "-L1GctConfigProducers,"
+        esmodules += "-L1MuTriggerScales,"
+        esmodules += "-MuonNumberingInitialization,"
+        esmodules += "-RPCGeometryESModule,"
+        esmodules += "-SiStripGainESProducer,"
+        esmodules += "-SiStripRecHitMatcherESProducer,"
+        esmodules += "-StripCPEfromTrackAngleESProducer,"
+        esmodules += "-TrackerDigiGeometryESModule,"
+        esmodules += "-TrackerGeometricDetESModule,"
+        esmodules += "-VolumeBasedMagneticFieldESProducer,"    
+        esmodules += "-ZdcHardcodeGeometryEP,"
+        esmodules += "-hcal_db_producer,"
+        esmodules += "-l1CaloGeometry,"
+        esmodules += "-l1CaloScales,"
+        esmodules += "-l1GtBoardMaps,"
+        esmodules += "-l1GtParameters,"
+        esmodules += "-l1GtPrescaleFactorsAlgoTrig,"
+        esmodules += "-l1GtPrescaleFactorsTechTrig,"
+        esmodules += "-l1GtStableParameters,"
+        esmodules += "-l1GtTriggerMaskAlgoTrig,"
+        esmodules += "-l1GtTriggerMaskTechTrig,"
+        esmodules += "-l1GtTriggerMaskVetoAlgoTrig,"
+        esmodules += "-l1GtTriggerMaskVetoTechTrig,"
+        esmodules += "-l1GtTriggerMenuXml,"
+        esmodules += "-sistripconn"
+
+    #essources += "-trackerAlignment,"
+    #essources += "-muonAlignment,"
+    #essources += "-siPixelGainCalibration,"
+    #essources += "-siPixelCabling,"
+    #essources += "-siPixelLorentzAngle,"
+    #essources += "-siStripApvGain,"
+    #essources += "-siStripNoise,"
+    #essources += "-siStripLorentzAngle,"
+    #essources += "-siStripFedCabling,"
+    #essources += "-SiStripPedestalsFakeESSource,"
+    #essources += "-maps_frontier,"
+    #essources += "-cscConditions,"
+    #essources += "-es_pool,"
+    #essources += "-ecalConditions,"
+    #essources += "-trackProbabilityFrontierCond,"
+    #essources += "-BTauMVAJetTagComputerRecord,"
+    #essources += "-BeamSpotEarlyCollision,"
+    #essources += "-DTCabling,"
+    #essources += "-RPCCabling,"
+    #essources += "-cscPackingCabling,"
+    #essources += "-cscUnpackingCabling,"
+    #essources += "-GlobalPosition,"
+
+
+    #essources += "-L1GtPrescaleFactorsRcdSource,"
+    #essources += "-L1GtTriggerMaskRcdSource,"
+    #essources += "-L1GtTriggerMenuRcdSource,"
+    #essources += "-L1MuGMTScalesRcdSource,"
+    #essources += "-l1GctConfigRecords,"
+    #essources += "-l1GctJcNegParsRecords,"
+    #essources += "-l1GctJcPosParsRecords,"
+    #essources += "-l1GctParamsRecords,"
+    #essources += "-tpparams,"
+    #essources += "-tpparams2,"
+    #essources += "-tpparams3,"
+    #essources += "-tpparams4,"
+    #essources += "-tpparams5,"
+    #essources += "-tpparams6,"
+    #essources += "-tpparams7,"
+    #essources += "-tpparams8,"
+    #essources += "-tpparams9,"
+    #essources += "-tpparams10,"
+    #essources += "-tpparams11,"
+
+
+
+    #esmodules += "-EcalTrigPrimESProducer,"
+    #esmodules += "-EcalTrigTowerConstituentsMapBuilder,"
+    #esmodules += "-L1MuGMTScales,"
+    #esmodules += "-l1GtFactors,"
+    # 200pre6 additions
+
+    modules = "--modules -hltTriggerSummaryAOD"
+
+    services = "--services "
+    services += "-PrescaleService,"
+    services += "-MessageLogger"
+
+    psets = "--psets "
+    psets += "-maxEvents,"
+    psets += "-configurationMetadata,"
+    psets += "-options,"
+
+    myGetCff = "edmConfigFromDB --cff --configName " + dbName + " " + essources + " " + esmodules + " " + modules + " " + services + " " + psets + " > " + cffName
+    os.system(myGetCff)
+
+    # myReplaceTrigResults = "replace TriggerResults::HLT " + process + " -- " + cffName
+    # os.system(myReplaceTrigResults)
+
+    # Make replace statements at the beginning of the cff
+    for line in fileinput.input(cffName,inplace=1):
+        if line.find("sequence HLTBeginSequence") >= 0:
+            print "// Begin replace statements specific to the HLT"
+            print "include \"HLTrigger/Configuration/data/HLTrigger_EventContent.cff\""
+            print "module hltTriggerSummaryAOD = TriggerSummaryProducerAOD {"
+            print "  string processName = \"@\""
+            print "  using TriggerSummaryAOD"
+            print "}"
+            print "// End replace statements specific to the HLT"  
+        print line[:-1]
