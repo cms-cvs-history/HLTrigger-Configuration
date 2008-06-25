38ce80cfc31f7e49eade1c09183f92fb27881efc
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-06-25 14:46:49 +0000

    Added first set of edited *_cfg.py files.

---
+++ test/TTbarGenSim_cfg.py
@@ -0,0 +1,82 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("GENSIM")
+
+
+process.load("Configuration.StandardSequences.Services_cff")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True),
+    Rethrow = cms.untracked.vstring('Unknown', 
+        'ProductNotFound', 
+        'DictionaryNotFound', 
+        'InsertFailure', 
+        'Configuration', 
+        'LogicError', 
+        'UnimplementedFeature', 
+        'InvalidReference', 
+        'NullPointerError', 
+        'NoProductSpecified', 
+        'EventTimeout', 
+        'EventCorruption', 
+        'ModuleFailure', 
+        'ScheduleExecutionFailure', 
+        'EventProcessorFailure', 
+        'FileInPathError', 
+        'FatalRootError', 
+        'NotFound')
+)
+
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+process.load("Configuration.Generator.PythiaUESettings_cfi")
+process.source = cms.Source("PythiaSource",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    PythiaParameters = cms.PSet(
+        process.pythiaUESettingsBlock,
+        processParameters = cms.vstring(
+            'MSEL      = 0     ! User defined processes', 
+            'MSUB(81)  = 1     ! qqbar to QQbar', 
+            'MSUB(82)  = 1     ! gg to QQbar', 
+            'MSTP(7)   = 6     ! flavour = top', 
+            'PMAS(6,1) = 175.  ! top quark mass'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring(
+            'pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.VtxSmearedEarly10TeVCollision_cff")
+
+# Conditions: fake or frontier
+# process.load("Configuration.StandardSequences.FakeConditions_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+# https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions
+process.GlobalTag.globaltag = 'STARTUP::All'
+
+process.load("Configuration.StandardSequences.Generator_cff")
+process.load("Configuration.StandardSequences.Sim_cff")
+
+process.gensim = cms.Path(process.pgen+process.psim)
+
+
+process.load("Configuration.EventContent.EventContent_cff")
+process.FEVT = cms.OutputModule("PoolOutputModule",
+    process.FEVTDEBUGHLTEventContent,
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM')
+    ),
+    fileName = cms.untracked.string('TTbarGenSim.root')
+)
+
+process.outpath = cms.EndPath(process.FEVT)
