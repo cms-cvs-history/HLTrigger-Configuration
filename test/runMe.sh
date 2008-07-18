3aeba7fe57716805c02fcda415268ab4f77bb3c6
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-07-18 12:25:45 +0000

    Cleanup and addition of py2Py translator for GUI import.

--- test/runMe.sh
+++ test/runMe.sh
@@ -17,40 +17,24 @@ echo "/bin/rm RelVal_Digi_Raw.root RelVal_Pure_Raw.root RelVal_Digi_Digi2Raw.log
       /bin/rm RelVal_Digi_Raw.root RelVal_Pure_Raw.root RelVal_Digi_Digi2Raw.log
 echo "cmsRun RelVal_Digi_Digi2Raw_cfg.py   >& RelVal_Digi_Digi2Raw.log"
       cmsRun RelVal_Digi_Digi2Raw_cfg.py   >& RelVal_Digi_Digi2Raw.log
-#echo "./testcfg RelVal_Digi_Digi2Raw.cfg   >& RelVal_Digi_Digi2Raw.log"
-#      ./testcfg RelVal_Digi_Digi2Raw.cfg   >& RelVal_Digi_Digi2Raw.log
-#echo "cmsRun --strict RelVal_Digi_Digi2Raw.cfg   >& RelVal_Digi_Digi2Raw.log"
-#      cmsRun --strict RelVal_Digi_Digi2Raw.cfg   >& RelVal_Digi_Digi2Raw.log
 
 echo " "
 echo "/bin/rm HLTFromPureRaw.root             RelVal_HLTFromRaw_2E30.log"
       /bin/rm HLTFromPureRaw.root             RelVal_HLTFromRaw_2E30.log
 echo "cmsRun RelVal_HLTFromRaw_2E30_cfg.py >& RelVal_HLTFromRaw_2E30.log"
       cmsRun RelVal_HLTFromRaw_2E30_cfg.py >& RelVal_HLTFromRaw_2E30.log
-#echo "./testcfg RelVal_HLTFromRaw_2E30.cfg >& RelVal_HLTFromRaw_2E30.log"
-#      ./testcfg RelVal_HLTFromRaw_2E30.cfg >& RelVal_HLTFromRaw_2E30.log
-#echo "cmsRun --strict RelVal_HLTFromRaw_2E30.cfg >& RelVal_HLTFromRaw_2E30.log"
-#      cmsRun --strict RelVal_HLTFromRaw_2E30.cfg >& RelVal_HLTFromRaw_2E30.log
 
 echo " "
 echo "/bin/rm HLTDe*.root                     OnLine_HLTFromRaw_2E30.log"
       /bin/rm HLTDe*.root                     OnLine_HLTFromRaw_2E30.log
 echo "cmsRun OnLine_HLTFromRaw_2E30_cfg.py >& OnLine_HLTFromRaw_2E30.log"
       cmsRun OnLine_HLTFromRaw_2E30_cfg.py >& OnLine_HLTFromRaw_2E30.log
-#echo "./testcfg OnLine_HLTFromRaw_2E30.cfg >& OnLine_HLTFromRaw_2E30.log"
-#      ./testcfg OnLine_HLTFromRaw_2E30.cfg >& OnLine_HLTFromRaw_2E30.log
-#echo "cmsRun --strict OnLine_HLTFromRaw_2E30.cfg >& OnLine_HLTFromRaw_2E30.log"
-#      cmsRun --strict OnLine_HLTFromRaw_2E30.cfg >& OnLine_HLTFromRaw_2E30.log
 
 echo " "
 echo "/bin/rm RelVal_Reco.root                RelVal_Reco.log"
       /bin/rm RelVal_Reco.root                RelVal_Reco.log
 echo "cmsRun RelVal_Reco_cfg.py            >& RelVal_Reco.log"
       cmsRun RelVal_Reco_cfg.py            >& RelVal_Reco.log
-#echo "./testcfg RelVal_Reco.cfg            >& RelVal_Reco.log"
-#      ./testcfg RelVal_Reco.cfg            >& RelVal_Reco.log
-#echo "cmsRun --strict RelVal_Reco.cfg            >& RelVal_Reco.log"
-#      cmsRun --strict RelVal_Reco.cfg            >& RelVal_Reco.log
 
 
 echo " "
