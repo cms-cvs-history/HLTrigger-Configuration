9768b2497bb0d0059ec52528d031eeb968d2cf9b
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-11-28 13:35:21 +0000

    FIX!

--- python/HLTDefaultOutput_cff.py
+++ python/HLTDefaultOutput_cff.py
@@ -1,1 +1,18 @@
-edmConfigFromDB: Command not found.
+# /dev/CMSSW_2_2_0/HLT/V2 (CMSSW_2_2_0)
+
+import FWCore.ParameterSet.Config as cms
+
+
+HLTConfigVersion = cms.PSet(
+  tableName = cms.string('/dev/CMSSW_2_2_0/HLT/V2')
+)
+
+block_hltDefaultOutput = cms.PSet(
+outputCommands = cms.untracked.vstring( 'drop *_hlt*_*_*',
+  'keep edmTriggerResults_*_*_*',
+  'keep triggerTriggerEvent_*_*_*',
+  'keep *_hltGctDigis_*_*',
+  'keep *_hltGtDigis_*_*',
+  'keep *_hltL1extraParticles_*_*',
+  'keep *_hltL1GtObjectMap_*_*' )
+)
