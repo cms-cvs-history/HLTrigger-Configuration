5b9ac130cae778160266e4830ee363c74d3c8fc5
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-11-28 13:24:05 +0000

    Bug fix in L1Seed

--- python/HLTDefaultOutput_cff.py
+++ python/HLTDefaultOutput_cff.py
@@ -1,18 +1,1 @@
-# /dev/CMSSW_2_2_0/HLT/V1 (CMSSW_2_2_0)
-
-import FWCore.ParameterSet.Config as cms
-
-
-HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_2_2_0/HLT/V1')
-)
-
-block_hltDefaultOutput = cms.PSet(
-outputCommands = cms.untracked.vstring( 'drop *_hlt*_*_*',
-  'keep edmTriggerResults_*_*_*',
-  'keep triggerTriggerEvent_*_*_*',
-  'keep *_hltGctDigis_*_*',
-  'keep *_hltGtDigis_*_*',
-  'keep *_hltL1extraParticles_*_*',
-  'keep *_hltL1GtObjectMap_*_*' )
-)
+edmConfigFromDB: Command not found.
--- test/getHLT.csh
+++ test/getHLT.csh
@@ -4,7 +4,7 @@ cmsenv
 
 rehash
 
-setenv HLTtable /dev/CMSSW_2_2_0/HLT/V1
+setenv HLTtable /dev/CMSSW_2_2_0/HLT/V2
 setenv HLTid    2E30
 
 if ($1 == CVS) then
