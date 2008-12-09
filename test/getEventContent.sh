71651ca34ff10de7dbb61de4dc2450b0e622e755
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-12-09 14:34:04 +0000

    Separate out event-content updates

---
+++ test/getEventContent.sh
@@ -0,0 +1,23 @@
+#!/bin/tcsh
+
+# usage: ./getEventContent.sh
+
+setenv HLTtable /dev/CMSSW_3_0_0/pre3/HLT
+
+cmsenv
+
+rehash
+
+ edmConfigFromDB --configName $HLTtable --nopaths --noes --nopsets --noservices --cff --blocks hltDefaultOutput::outputCommands >& HLTDefaultOutput_cff.py
+ /bin/mv -f HLTDefaultOutput_cff.py ../python
+
+ edmConfigFromDB --configName $HLTtable --nopaths --noes --nopsets --noservices --cff --blocks hltDefaultOutputWithFEDs::outputCommands >& HLTDefaultOutputWithFEDs_cff.py
+ /bin/mv -f HLTDefaultOutputWithFEDs_cff.py ../python
+
+ edmConfigFromDB --configName $HLTtable --nopaths --noes --nopsets --noservices --cff --blocks hltDebugOutput::outputCommands >& HLTDebugOutput_cff.py
+ /bin/mv -f HLTDebugOutput_cff.py ../python
+
+ edmConfigFromDB --configName $HLTtable --nopaths --noes --nopsets --noservices --cff --blocks hltDebugWithAlCaOutput::outputCommands >& HLTDebugWithAlCaOutput_cff.py
+ /bin/mv -f HLTDebugWithAlcaOutput_cff.py ../python
+
+unsetenv HLTtable
