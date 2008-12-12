8261dab14e5f93fa7aecd74423813c49c263d118
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2008-12-12 09:54:48 +0000

    Fix Raw data file generic name.

--- test/runMe.sh
+++ test/runMe.sh
@@ -13,6 +13,7 @@ echo "Creating  online configs from ConfDB"
       /bin/rm OnLine_HLT_?E??_cfg.py
 echo "./getHLT.sh"
       ./getHLT.sh 
+/bin/ln -s RelVal_DigiL1Raw.root RelVal_Pure_Raw.root
 
 foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLT_2E30 OnLine_HLT_8E29 OnLine_HLT_1E31 RelVal_DigiL1RawHLT RelVal_HLT2 RelVal_L1HLT2 RelVal_Reco )
     echo " "
