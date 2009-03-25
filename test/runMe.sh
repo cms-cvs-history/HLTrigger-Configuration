79cb41b08a8ebd5108a24e8cc116441b8306e7b7
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2009-03-25 08:17:25 +0000

    Further misc. updates for tags

--- test/runMe.sh
+++ test/runMe.sh
@@ -16,7 +16,8 @@ echo "./getHLT.sh"
       ./getHLT.sh 
 
 foreach lumi ( 8E29 1E31 )
-foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_DigiL1RawHLT RelVal_HLT2 RelVal_L1HLT2 RelVal_Reco )
+# foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_DigiL1RawHLT RelVal_HLT2 RelVal_L1HLT2 RelVal_Reco )
+foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_HLT2 )
     echo " "
     set name = ${task}_${lumi}
     foreach ext (log root)
