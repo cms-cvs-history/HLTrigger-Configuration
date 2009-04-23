2a3148f74c7fec547ed35de69228f4a27ceec077
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2009-04-23 16:24:38 +0000

    make the script nicer

--- test/getHLT.sh
+++ test/getHLT.sh
@@ -5,12 +5,24 @@ HLTtable8E29="/dev/CMSSW_2_2_6_HLT/8E29/V37"
 HLTtable1E31="/dev/CMSSW_2_2_6_HLT/1E31/V37"
 HLTtableFULL="/dev/CMSSW_2_2_6_HLT/merged/V37"
 
+# getHLT.py
+PACKAGE="HLTrigger/Configuration"
+if [ -f "./getHLT.py" ]; then
+  GETHLT="./getHLT.py"
+elif [ -f "$CMSSW_BASE/src/$PACKAGE/test/getHLT.py" ]; then
+  GETHLT="$CMSSW_BASE/src/$PACKAGE/test/getHLT.py"
+elif [ -f "$CMSSW_RELEASE_BASE/src/$PACKAGE/test/getHLT.py" ]; then
+  GETHLT="$CMSSW_RELEASE_BASE/src/$PACKAGE/test/getHLT.py"
+else
+  echo "cannot find getHLT.py, aborting"
+  exit 1
+fi
 
 function getConfigForCVS() {
   # for things in CMSSW CVS
   local HLTtable="$1"
   local HLTid="$2"
-  ./getHLT.py $HLTtable $HLTid GEN-HLT
+  $GETHLT $HLTtable $HLTid GEN-HLT
 }
 
 function getContentForCVS() {
@@ -25,24 +37,26 @@ function getConfigForOnline() {
   # for things NOT in CMSSW CVS:
   local HLTtable="$1"
   local HLTid="$2"
-  ./getHLT.py $HLTtable $HLTid
+  $GETHLT $HLTtable $HLTid
 }
 
-
+# make sure we're using *these* working area
 eval `scramv1 runtime -sh`
 hash -r
 
 if [ "$1" == "CVS" ]; then
   # for things in CMSSW CVS
+  rm -f HLT*_cff.py
   getConfigForCVS "${HLTtable8E29}" "8E29"
   getConfigForCVS "${HLTtable1E31}" "1E31"
   getConfigForCVS "${HLTtableFULL}" "FULL"
   getContentForCVS "${HLTtableFULL}"
-
   ls -lt HLT*_cff.py
   mv -f HLT*_cff.py ../python
 else
   # for things NOT in CMSSW CVS:
+  rm -f OnLine_HLTFromRaw_*_cfg.py
   getConfigForOnline "${HLTtable8E29}" "8E29"
   getConfigForOnline "${HLTtable1E31}" "1E31"
+  ls -lt OnLine_HLTFromRaw_*_cfg.py
 fi
