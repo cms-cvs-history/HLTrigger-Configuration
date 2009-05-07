3d8b45a9c31d0b16e31fc3b5bf3f066095a1d414
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2009-05-07 17:05:08 +0000

    Restore STARTUP_V9 for comparison

--- test/cmsDriver.sh
+++ test/cmsDriver.sh
@@ -8,28 +8,28 @@ cmsDriver.py TTbar_Tauola.cfi --step=GEN,SIM --fileout=TTbarGenSim.root --number
 
 echo
 echo "Creating TTbarGenHLT for 2E30"
-cmsDriver.py TTbar_Tauola.cfi --step=GEN,SIM,DIGI,L1,DIGI2RAW,HLT --conditions=FrontierConditions_GlobalTag,IDEAL_V12::All                                                --fileout=TTbarGenHLT_2E30.root         --number=100 --mc --no_exec --datatier 'GEN-SIM-DIGI-RAW-HLT' --eventcontent=FEVTDEBUGHLT --customise=HLTrigger/Configuration/customL1THLT_Options.py   --python_filename=TTbarGenHLT_2E30.py
+cmsDriver.py TTbar_Tauola.cfi --step=GEN,SIM,DIGI,L1,DIGI2RAW,HLT --conditions=FrontierConditions_GlobalTag,STARTUP_V9::All                                                --fileout=TTbarGenHLT_2E30.root         --number=100 --mc --no_exec --datatier 'GEN-SIM-DIGI-RAW-HLT' --eventcontent=FEVTDEBUGHLT --customise=HLTrigger/Configuration/customL1THLT_Options.py   --python_filename=TTbarGenHLT_2E30.py
 
 echo
 echo "Creating DigiL1Raw for 2E30"
-cmsDriver.py RelVal --step=DIGI,L1,DIGI2RAW                       --conditions=FrontierConditions_GlobalTag,IDEAL_V12::All --filein=file:TTbarGenSim.root                 --fileout=RelVal_DigiL1Raw_2E30.root    --number=100 --mc --no_exec --datatier 'GEN-SIM-DIGI-RAW'     --eventcontent=RAW          --customise=HLTrigger/Configuration/customL1T_Options.py      --python_filename=RelVal_DigiL1Raw_2E30.py
+cmsDriver.py RelVal --step=DIGI,L1,DIGI2RAW                       --conditions=FrontierConditions_GlobalTag,STARTUP_V9::All --filein=file:TTbarGenSim.root                 --fileout=RelVal_DigiL1Raw_2E30.root    --number=100 --mc --no_exec --datatier 'GEN-SIM-DIGI-RAW'     --eventcontent=RAW          --customise=HLTrigger/Configuration/customL1T_Options.py      --python_filename=RelVal_DigiL1Raw_2E30.py
 
 echo
 echo "Creating DigiL1RawHLT for 2E30"
-cmsDriver.py RelVal --step=DIGI,L1,DIGI2RAW,HLT                   --conditions=FrontierConditions_GlobalTag,IDEAL_V12::All --filein=file:TTbarGenSim.root                 --fileout=RelVal_DigiL1RawHLT_2E30.root --number=100 --mc --no_exec --datatier 'GEN-SIM-DIGI-RAW-HLT' --eventcontent=FEVTDEBUGHLT --customise=HLTrigger/Configuration/customL1THLT_Options.py   --python_filename=RelVal_DigiL1RawHLT_2E30.py
+cmsDriver.py RelVal --step=DIGI,L1,DIGI2RAW,HLT                   --conditions=FrontierConditions_GlobalTag,STARTUP_V9::All --filein=file:TTbarGenSim.root                 --fileout=RelVal_DigiL1RawHLT_2E30.root --number=100 --mc --no_exec --datatier 'GEN-SIM-DIGI-RAW-HLT' --eventcontent=FEVTDEBUGHLT --customise=HLTrigger/Configuration/customL1THLT_Options.py   --python_filename=RelVal_DigiL1RawHLT_2E30.py
 
 echo
 echo "Creating HLT for 2E30"
-cmsDriver.py RelVal --step=L1,HLT                                 --conditions=FrontierConditions_GlobalTag,IDEAL_V12::All --filein=file:RelVal_DigiL1Raw_2E30.root      --fileout=RelVal_HLT_2E30.root          --number=100 --mc --no_exec --datatier 'RAW-HLT'              --eventcontent=FEVTDEBUGHLT --customise=HLTrigger/Configuration/customHLT_Options.py      --python_filename=RelVal_HLT_2E30.py
+cmsDriver.py RelVal --step=L1,HLT                                 --conditions=FrontierConditions_GlobalTag,STARTUP_V9::All --filein=file:RelVal_DigiL1Raw_2E30.root      --fileout=RelVal_HLT_2E30.root          --number=100 --mc --no_exec --datatier 'RAW-HLT'              --eventcontent=FEVTDEBUGHLT --customise=HLTrigger/Configuration/customHLT_Options.py      --python_filename=RelVal_HLT_2E30.py
 
 echo
 echo "Creating HLT2 (re-running HLT) for 2E30"
-cmsDriver.py RelVal --step=L1,HLT                                 --conditions=FrontierConditions_GlobalTag,IDEAL_V12::All --filein=file:RelVal_HLT_2E30.root            --fileout=RelVal_HLT2_2E30.root         --number=100 --mc --no_exec --datatier 'RAW-HLT'              --eventcontent=FEVTDEBUGHLT --customise=HLTrigger/Configuration/customHLT2_Options.py     --python_filename=RelVal_HLT2_2E30.py
+cmsDriver.py RelVal --step=L1,HLT                                 --conditions=FrontierConditions_GlobalTag,STARTUP_V9::All --filein=file:RelVal_HLT_2E30.root            --fileout=RelVal_HLT2_2E30.root         --number=100 --mc --no_exec --datatier 'RAW-HLT'              --eventcontent=FEVTDEBUGHLT --customise=HLTrigger/Configuration/customHLT2_Options.py     --python_filename=RelVal_HLT2_2E30.py
 
 echo
 echo "Creating L1HLT2 (re-running L1 and HLT) for 2E30"
-cmsDriver.py RelVal --step=L1,HLT                                 --conditions=FrontierConditions_GlobalTag,IDEAL_V12::All --filein=file:RelVal_DigiL1RawHLT_2E30.root   --fileout=RelVal_L1HLT2_2E30.root       --number=100 --mc --no_exec --datatier 'GEN-SIM-DIGI-RAW-HLT' --eventcontent=FEVTDEBUGHLT --customise=HLTrigger/Configuration/customL1THLT2_Options.py  --python_filename=RelVal_L1HLT2_2E30.py
+cmsDriver.py RelVal --step=L1,HLT                                 --conditions=FrontierConditions_GlobalTag,STARTUP_V9::All --filein=file:RelVal_DigiL1RawHLT_2E30.root   --fileout=RelVal_L1HLT2_2E30.root       --number=100 --mc --no_exec --datatier 'GEN-SIM-DIGI-RAW-HLT' --eventcontent=FEVTDEBUGHLT --customise=HLTrigger/Configuration/customL1THLT2_Options.py  --python_filename=RelVal_L1HLT2_2E30.py
 
 echo
 echo "Creating Reco for 2E30"
-cmsDriver.py RelVal --step=RAW2DIGI,RECO                          --conditions=FrontierConditions_GlobalTag,IDEAL_V12::All --filein=file:RelVal_DigiL1Raw_2E30.root      --fileout=RelVal_Reco_2E30.root         --number=100 --mc --no_exec --datatier 'RECO'                 --eventcontent=RECOSIM      --customise=HLTrigger/Configuration/custom_Options.py         --python_filename=RelVal_Reco_2E30.py
+cmsDriver.py RelVal --step=RAW2DIGI,RECO                          --conditions=FrontierConditions_GlobalTag,STARTUP_V9::All --filein=file:RelVal_DigiL1Raw_2E30.root      --fileout=RelVal_Reco_2E30.root         --number=100 --mc --no_exec --datatier 'RECO'                 --eventcontent=RECOSIM      --customise=HLTrigger/Configuration/custom_Options.py         --python_filename=RelVal_Reco_2E30.py
