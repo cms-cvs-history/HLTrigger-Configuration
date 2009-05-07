f3ecc9ae82255144d5dc6c8d323cb025e63fc480
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2009-05-07 11:48:34 +0000

    Fixup Andrea's fallout

--- test/runMe.sh
+++ test/runMe.sh
@@ -12,8 +12,11 @@ echo "Creating  online configs from ConfDB"
 rm -f OnLine_HLT_*.py
 ./getHLT.sh 
 
+/bin/ln -s RelVal_DigiL1Raw_2E30.root RelVal_Pure_Raw.root
+/bin/ln -s /scratch/cms/TTbarGenSim2110.root TTbarGenSim.root
+
 # foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_DigiL1RawHLT RelVal_HLT2 RelVal_L1HLT2 RelVal_Reco )
-foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_HLT2 )
+foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLTFromRaw RelVal_HLT2 )
     echo
     set name = ${task}_2E30
     foreach ext (log root)
