5bc1fc6868875177afe1dda45173a89f67fc0e80
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2009-11-04 12:35:13 +0000

    *** empty log message ***

--- test/runMe.sh
+++ test/runMe.sh
@@ -15,10 +15,23 @@ echo "./cmsDriver.sh"
 #echo "./getHLT.sh"
 #      ./getHLT.sh 
 
-# GRun = 8E29+MWGR
-foreach lumi ( 8E29 GRun 1E31 HIon )
+# GRun = 8E29+MWGRcommissioning
 # foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_DigiL1RawHLT RelVal_HLT2 RelVal_L1HLT2 RelVal_Reco )
-foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_HLT2 )
+
+foreach lumi ( 8E29 GRun 1E31 HIon )
+  foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_HLT2 )
+    echo " "
+    set name = ${task}_${lumi}
+    foreach ext (log root)
+	/bin/rm $name.$ext
+    end
+    echo "cmsRun $name.py >& $name.log"
+          cmsRun $name.py >& $name.log
+  end
+end
+
+foreach lumi ( 8E29 1E31 )
+  foreach task ( RelVal_Reco )
     echo " "
     set name = ${task}_${lumi}
     foreach ext (log root)
