2e15df385ee059d5c419f1d19a994e1dc620c8f9
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2009-11-28 11:36:15 +0000

    Removing PrescaleService

--- test/runMe.sh
+++ test/runMe.sh
@@ -5,20 +5,19 @@ cmsenv
 rehash
 
 echo " "
+echo "Removing PrescaleService from online configs"
+foreach lumi ( 8E29 GRun 1E31 HIon )
+    echo "del process.PrescaleService" >> OnLine_HLT_${lumi}.py
+end
+
+echo " "
 echo "Creating offline configs with cmsDriver"
 echo "./cmsDriver.sh"
       ./cmsDriver.sh
 
-#echo " "
-#echo "Creating  online configs from ConfDB"
-#      /bin/rm OnLine_HLT_????.py
-#echo "./getHLT.sh"
-#      ./getHLT.sh 
-
-# GRun = 8E29+MWGRcommissioning
-# foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_DigiL1RawHLT RelVal_HLT2 RelVal_L1HLT2 RelVal_Reco )
-
+# GRun = 8E29+MWGR
 foreach lumi ( 8E29 GRun 1E31 HIon )
+# foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_DigiL1RawHLT RelVal_HLT2 RelVal_L1HLT2 RelVal_Reco )
   foreach task ( RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_HLT2 )
     echo " "
     set name = ${task}_${lumi}
