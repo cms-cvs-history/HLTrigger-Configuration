378ef605a9b3325a9622fc73d90a480bf7b3e72d
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2010-02-09 14:50:08 +0000

    Adding process.GlobalTag.pfnPrefix=cms.untracked.string('frontier://FrontierProd/') for re-running on data offline

--- test/getHLT.py
+++ test/getHLT.py
@@ -384,6 +384,8 @@ process.es_prefer_Level1MenuOverride = cms.ESPrefer( "PoolDBESSource", "Level1Me
           out.write("    process.GlobalTag.globaltag         = '%s'\n" % menuGlobalTag)
           out.write("    process.GlobalTag.connect           = 'frontier://FrontierProd/CMS_COND_31X_GLOBALTAG'\n")
           out.write("\n")
+          if runOnData:
+              out.write("process.GlobalTag.pfnPrefix=cms.untracked.string('frontier://FrontierProd/')\n")
           out.write("if 'Level1MenuOverride' in process.__dict__:\n")
           out.write("    process.Level1MenuOverride.connect  = 'frontier://FrontierProd/CMS_COND_31X_L1T'\n")
           out.write("\n")
