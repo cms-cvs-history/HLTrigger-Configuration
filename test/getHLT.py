735907f2b8c246ec90628519a2ddb17c6a56dbcb
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2010-03-02 06:24:19 +0000

    Update for new real-data global tag.

--- test/getHLT.py
+++ test/getHLT.py
@@ -18,7 +18,7 @@ l1Override = {
 globalTag = {
   '8E29': 'START3X_V24::All',
   'GRun': 'START3X_V24::All',
-  'data': 'GR10_H_V2::All',
+  'data': 'GR10_H_V3C::All',
   '1E31': 'MC_3XY_V24::All',
   'HIon': 'MC_3XY_V24::All',
   None:   'START3X_V24::All',      # use as default
