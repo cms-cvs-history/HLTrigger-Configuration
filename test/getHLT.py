318e385bce1e11e2b0f7c3c3cdc49c82738a8879
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2010-03-09 12:48:25 +0000

    Delaysed expansion of autoCond

--- test/getHLT.py
+++ test/getHLT.py
@@ -380,13 +380,13 @@ process.es_prefer_Level1MenuOverride = cms.ESPrefer( "PoolDBESSource", "Level1Me
               menuGlobalTag = globalTag[fileId]
             else:
               menuGlobalTag = globalTag[None]
-          if 'auto:' in menuGlobalTag:
-            from Configuration.PyReleaseValidation.autoCond import autoCond
-            for ac,cond in autoCond.items():
-              menuGlobalTag = menuGlobalTag.replace('auto:'+ac,cond)
           out.write("if 'GlobalTag' in process.__dict__:\n")
           out.write("    process.GlobalTag.globaltag         = '%s'\n" % menuGlobalTag)
           out.write("    process.GlobalTag.connect           = 'frontier://FrontierProd/CMS_COND_31X_GLOBALTAG'\n")
+          out.write("    if 'auto:' in process.GlobalTag.globaltag:\n")
+          out.write("        from Configuration.PyReleaseValidation.autoCond import autoCond\n")
+          out.write("        for ac,cond in autoCond.items():\n")
+          out.write("            process.GlobalTag.globaltag = process.GlobalTag.globaltag.replace('auto:'+ac,cond)\n")
           if runOnData:
             out.write("    process.GlobalTag.pfnPrefix         = cms.untracked.string('frontier://FrontierProd/')\n")
           out.write("\n")
