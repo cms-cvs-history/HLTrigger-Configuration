5bb57fd8e269243c14d38eb475f6f29367a2e33d
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2010-04-01 21:23:16 +0000

    Fix prescale

--- test/OnLine_HLT_1E31.py
+++ test/OnLine_HLT_1E31.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_3_6_0/pre4/1E31/V12 (CMSSW_3_6_X_2010-04-01-0100_HLT1)
+# /dev/CMSSW_3_6_0/pre4/1E31/V13 (CMSSW_3_6_X_2010-04-01-0100_HLT1)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLT" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_6_0/pre4/1E31/V12')
+  tableName = cms.string('/dev/CMSSW_3_6_0/pre4/1E31/V13')
 )
 
 process.options = cms.untracked.PSet(  Rethrow = cms.untracked.vstring( 'ProductNotFound',
