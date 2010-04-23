8c5241a29f4081493adada54ba9607e851adf7cf
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2010-04-23 06:23:25 +0000

    Update of globaltag for real data.

--- test/getHLT.py
+++ test/getHLT.py
@@ -9,7 +9,7 @@ import fileinput
 globalTag = {
   '8E29': 'auto:startup',
   'GRun': 'auto:startup',
-  'data': 'GR10_H_V5::All',
+  'data': 'GR10_36XH_V5::All',
   '1E31': 'auto:mc',
   'HIon': 'auto:mc',
   None:   'auto:startup',      # use as default
