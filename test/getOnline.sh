9bb2b2e4095d4ededfd581a8f43fc2651e8a6655
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-05-04 19:54:42 +0000

    override L1 menus

--- test/getOnline.sh
+++ test/getOnline.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
-HLT='/online/collisions/2010/week15/HLT'
+HLT='/online/collisions/2010/week19/HLT'
+L1T='L1Menu_Commissioning2010_v2'
 
 rm -f OnData_HLT_TEST.py
 rm -f OnLine_HLT_TEST.py
 
-./getHLT.py --process TEST --full --offline --mc   $HLT TEST
+./getHLT.py --process TEST --full --offline --mc   --l1 $L1T $HLT TEST
 mv OnLine_HLT_TEST.py offline_mc.py
-./getHLT.py --process TEST --full --offline --data $HLT TEST
+./getHLT.py --process TEST --full --offline --data --l1 $L1T $HLT TEST
 mv OnData_HLT_TEST.py offline_data.py
-./getHLT.py --process TEST --full --online  --data $HLT TEST
+./getHLT.py --process TEST --full --online  --data --l1 $L1T $HLT TEST
 mv OnData_HLT_TEST.py online_data.py
 
 {
