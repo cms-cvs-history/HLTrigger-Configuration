947ee282ff45d112177c0307185b41a8f1449619
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2010-07-07 15:23:22 +0000

    Remove Conor's broken path until he gets his act together.

--- test/OnLine_HLT_1E31.py
+++ test/OnLine_HLT_1E31.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_3_6_2/1E31/V29 (CMSSW_3_6_2_HLT8)
+# /dev/CMSSW_3_6_2/1E31/V30 (CMSSW_3_6_2_HLT8)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLT" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_6_2/1E31/V29')
+  tableName = cms.string('/dev/CMSSW_3_6_2/1E31/V30')
 )
 
 process.options = cms.untracked.PSet(  Rethrow = cms.untracked.vstring( 'ProductNotFound',
