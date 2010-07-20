915f584a04b70cdeb9dd8cf2a54c911153c9418c
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-07-20 13:16:04 +0000

    fix IsoTrack parameters

--- test/OnData_HLT_1E31.py
+++ test/OnData_HLT_1E31.py
@@ -1,11 +1,11 @@
-# /dev/CMSSW_3_6_2/1E31/V34 (CMSSW_3_6_2_HLT9)
+# /dev/CMSSW_3_6_2/1E31/V35 (CMSSW_3_6_2_HLT9)
 
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process( "HLT" )
 
 process.HLTConfigVersion = cms.PSet(
-  tableName = cms.string('/dev/CMSSW_3_6_2/1E31/V34')
+  tableName = cms.string('/dev/CMSSW_3_6_2/1E31/V35')
 )
 
 process.options = cms.untracked.PSet(  Rethrow = cms.untracked.vstring( 'ProductNotFound',
@@ -30,12 +30,12 @@ process.streams = cms.PSet(
     'JetMETTauMonitor',
     'HcalHPDNoise',
     'HcalNZS',
-    'MinimumBias',
     'MuOnia',
     'EGMonitor',
     'RandomTriggers',
     'EG',
-    'Mu' ),
+    'Mu',
+    'MinimumBias' ),
   EventDisplay = cms.vstring(  ),
   Express = cms.vstring( 'ExpressPhysics' ),
   HLTDQM = cms.vstring(  )
@@ -106,9 +106,6 @@ process.datasets = cms.PSet(
   JetMETTauMonitor = cms.vstring( 'HLT_L1MET20' ),
   HcalHPDNoise = cms.vstring(  ),
   HcalNZS = cms.vstring( 'HLT_HcalPhiSym' ),
-  MinimumBias = cms.vstring( 'HLT_L1Tech_BSC_HighMultiplicity',
-    'HLT_ZeroBiasPixel_SingleTrack',
-    'HLT_PixelTracks_Multiplicity70' ),
   MuOnia = cms.vstring( 'HLT_Mu0_L1MuOpen',
     'HLT_Mu0_Track0_Jpsi',
     'HLT_Mu3_L1MuOpen',
@@ -137,6 +134,9 @@ process.datasets = cms.PSet(
     'HLT_Mu5',
     'HLT_Mu9',
     'HLT_L2Mu11' ),
+  MinimumBias = cms.vstring( 'HLT_L1Tech_BSC_HighMultiplicity',
+    'HLT_ZeroBiasPixel_SingleTrack',
+    'HLT_PixelTracks_Multiplicity70' ),
   ExpressPhysics = cms.vstring( 'HLT_MET100',
     'HLT_ZeroBias',
     'HLT_L1SingleEG5',
