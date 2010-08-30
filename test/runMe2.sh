f7d8c6bc61a46e5167353b7b925a383e271b713d
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2010-08-30 21:40:10 +0000

    adapt to new GlobalTag (use GRun menu for both startup and mc)

--- test/runMe2.sh
+++ test/runMe2.sh
@@ -1,49 +1,11 @@
 #! /bin/bash
 
-echo
-echo "Removing prescales from online configs"
-for task in OnData OnLine; do
-  for lumi in 8E29 GRun 1E31 HIon; do
-    cat >> ${task}_HLT_${lumi}.py <<EOF
-
-# remove HLT prescales
-if 'PrescaleService' in process.__dict__:
-    process.PrescaleService.lvl1DefaultLabel = cms.untracked.string( '0' )
-    process.PrescaleService.lvl1Labels = cms.vstring( '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' )
-    process.PrescaleService.prescaleTable = cms.VPSet( )
-
-EOF
-  done
-done
-
-echo
 echo "Creating offline configs with cmsDriver"
 echo "./cmsDriver.sh"
       ./cmsDriver.sh
 
-#for lumi in GRun; do
-#  for task in OnLine_HLT RelVal_HLT RelVal_HLT2; do 
-#  cat >> ${task}_${lumi}.py <<EOF
-#
-## L1 not yet in GlobalTag - add by hand!
-#from CondCore.DBCommon.CondDBSetup_cfi import *
-#process.newL1menu = cms.ESSource(
-#   "PoolDBESSource",CondDBSetup,
-#   connect = cms.string("frontier://FrontierProd/CMS_COND_31X_L1T"),
-#   toGet = cms.VPSet(cms.PSet(record = cms.string("L1GtTriggerMenuRcd"),
-#                              tag = cms.string("L1GtTriggerMenu_STARTUP_v6"))
-#                     )
-#   )
-#process.es_prefer_newL1menu = cms.ESPrefer("PoolDBESSource","newL1menu")
-#
-#EOF
-#  done
-#done
-
-# GRun = 8E29+MWGR
-for lumi in 8E29 GRun 1E31 HIon; do
-# for task in RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_DigiL1RawHLT RelVal_HLT2 RelVal_L1HLT2 RelVal_Reco; do
-  for task in RelVal_DigiL1Raw RelVal_HLT OnLine_HLT RelVal_HLT2 OnData_HLT; do
+for lumi in GRun HIon; do
+  for task in OnLine_HLT OnData_HLT; do
     echo
     name=${task}_${lumi}
     rm -f $name.{log,root}
@@ -52,10 +14,10 @@ for lumi in 8E29 GRun 1E31 HIon; do
   done &        # run the different lumi in parallel
 done
 
-wait            # wail for all process to complete
+wait            # wait for all process to complete
 
-for lumi in 8E29 1E31; do
-  for task in RelVal_Reco; do
+for lumi in 8E29 1E31 HIon; do
+  for task in RelVal_DigiL1Raw RelVal_HLT RelVal_HLT2 RelVal_Reco; do
     echo
     name=${task}_${lumi}
     rm -f $name.{log,root}
@@ -64,4 +26,4 @@ for lumi in 8E29 1E31; do
   done &        # run the different lumi in parallel
 done
 
-wait            # wail for all process to complete
+wait            # wait for all process to complete
