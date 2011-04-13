52be93171e1bc6df8fe9f6b45231e36446c79c7b
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2011-04-13 05:59:56 +0000

    globaltag

--- test/getOnline.sh
+++ test/getOnline.sh
@@ -3,9 +3,9 @@
 HLT='/online/collisions/2011/5e32/v8.0/HLT'
 L1T='L1GtTriggerMenu_L1Menu_Collisions2011_v1_mc,frontier://FrontierProd/CMS_COND_31X_L1T'
 
-hltGetConfiguration $HLT --process TEST --l1 $L1T --full --offline --mc   --unprescale > offline_mc.py
-hltGetConfiguration $HLT --process TEST --l1 $L1T --full --offline --data --unprescale > offline_data.py
-hltGetConfiguration $HLT --process TEST --l1 $L1T --full --online  --data --unprescale > online_data.py
+hltGetConfiguration $HLT --process TEST --l1 $L1T --globaltag L1HLTST311_V0::All --full --offline --mc   --unprescale > offline_mc.py
+hltGetConfiguration $HLT --process TEST --l1 $L1T --globaltag GRP311L1HLTV0::All --full --offline --data --unprescale > offline_data.py
+hltGetConfiguration $HLT --process TEST --l1 $L1T                                --full --online  --data --unprescale > online_data.py
 
 {
   TABLE=$(echo $HLT | cut -d: -f2)
