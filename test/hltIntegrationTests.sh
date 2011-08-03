481956eddf97de92e0319a15ce949357b8416520
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2011-08-03 23:15:42 +0000

    L1GtTriggerMenu_L1Menu_Collisions2011_v5_mc should now be available on Oracle

--- test/hltIntegrationTests.sh
+++ test/hltIntegrationTests.sh
@@ -6,8 +6,8 @@ rehash
 
 set InputFile = file:/tmp/fwyzard/B814262C-EE97-E011-94FB-003048F118AA.root
 
-hltIntegrationTests /dev/CMSSW_4_2_0/GRun -d hltIntegrationTests -i $InputFile -n 100 -j 2      -x "--l1-emulator" -x "--l1 L1GtTriggerMenu_L1Menu_Collisions2011_v5_mc,sqlite_file:/afs/cern.ch/user/g/ghete/public/L1Menu/L1Menu_Collisions2011_v5/sqlFile/L1Menu_Collisions2011_v5_mc.db"
+hltIntegrationTests /dev/CMSSW_4_2_0/GRun -d hltIntegrationTests -i $InputFile -n 100 -j 2      -x "--l1-emulator" -x "--l1 L1GtTriggerMenu_L1Menu_Collisions2011_v5_mc"
 
 set InputFile =  file:../RelVal_DigiL1Raw_GRun.root
 
-hltIntegrationTests /dev/CMSSW_4_2_0/GRun -d hltIntegrationTests -i $InputFile -n 100 -j 2 --mc -x "--l1-emulator" -x "--l1 L1GtTriggerMenu_L1Menu_Collisions2011_v5_mc,sqlite_file:/afs/cern.ch/user/g/ghete/public/L1Menu/L1Menu_Collisions2011_v5/sqlFile/L1Menu_Collisions2011_v5_mc.db"
+hltIntegrationTests /dev/CMSSW_4_2_0/GRun -d hltIntegrationTests -i $InputFile -n 100 -j 2 --mc -x "--l1-emulator" -x "--l1 L1GtTriggerMenu_L1Menu_Collisions2011_v5_mc"
